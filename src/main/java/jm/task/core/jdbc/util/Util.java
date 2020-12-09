6f25db6edd292e13200a3be6467919e83b5fe21c
UlugbekSatymov <satymov.u@gmail.com>
2020-12-09 03:24:43 +0300

    Hibernate without xml

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -14,12 +14,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-
 public class Util{
     private final static String URL = "jdbc:mysql://localhost:3306/dbtest?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC";
     private final static String USER = "root";
     private final static String PASSWORD = "root";
-    private final static String FILENAME = "src/main/java/recources/hibernate.cfg.xml";
+//    private final static String FILENAME = "src/main/java/recources/hibernate.cfg.xml";
 
 
 
@@ -28,13 +27,47 @@ public class Util{
     }
 
 
-    public static SessionFactory getSessionFactory() {
-        Configuration config = new Configuration().configure(new File(FILENAME));
-        config.addAnnotatedClass(User.class);
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(config.getProperties());
-        return config.buildSessionFactory(builder.build());
-    }
+//    public static SessionFactory getSessionFactory() {
+//        Configuration config = new Configuration().configure(new File(FILENAME));
+//        config.addAnnotatedClass(User.class);
+//        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+//                .applySettings(config.getProperties());
+//        return config.buildSessionFactory(builder.build());
+//    }
+        private static SessionFactory sessionFactory;
+
+        public static SessionFactory getSessionFactory() {
+            if (sessionFactory == null) {
+                try {
+                    Configuration configuration = new Configuration();
+
+                    Properties settings = new Properties();
+                    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/dbtest?serverTimezone=UTC");
+                    settings.put(Environment.USER, "root");
+                    settings.put(Environment.PASS, "root");
+                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                    settings.put(Environment.SHOW_SQL, "true");
+
+                    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                    configuration.setProperties(settings);
+
+                    configuration.addAnnotatedClass(User.class);
+
+                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties()).build();
+
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return sessionFactory;
+        }
 
 
 }
